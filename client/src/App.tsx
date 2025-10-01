98c5e61f57e5d104d627e4732f289a661d63f2cb
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-10-01 14:43:47 +0500

    Подключил файлы

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import "./styles/_reboot.scss";
+import "./styles/_container.scss";
+import "./styles/_variables.scss";
+import "./styles/_fonts.scss";
+import "./styles/_btn.scss";
+
+import { Header } from "./Components/Header/Header";
 
 function App() {
-
   return (
     <>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
