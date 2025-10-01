e8c2bba6137900993e0d92929e1629563e33f0cc
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-10-01 14:14:36 +0500

    Добавил types

---
+++ client/src/Components/Types/Types.tsx
@@ -0,0 +1,49 @@
+export type ProductCard = {
+  id: number;
+  image: string;
+  name: string;
+  price: {
+    new: number;
+    old: number;
+  };
+  info: string;
+  availability: {
+    moscow: number;
+    orenburg: number;
+    saintPetersburg: number;
+  };
+  goodsOfDay: boolean;
+}
+
+export type AccordingProps = {
+  index: number;
+  isOpen: boolean;
+  onToggle: () => void;
+  question: string;
+  answer1: string;
+  answer2?: string;
+}
+
+export type HeaderLocationProps = {
+  city: string;
+  isOpenLocation: boolean;
+  onLocationClose: () => void;
+  onCityChange: (city: string) => void;
+}
+
+export type HeaderProps  = {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export type HeaderBasketProps = {
+  isOpenBasket: boolean;
+  onBasketClose: () => void;
+}
+
+// Catalog
+export type CatalogRender = {
+  products: ProductCard[];
+  productCount: number;
+  currentPage: number;
+}
