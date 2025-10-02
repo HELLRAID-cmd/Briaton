e322f9fa04c7593b2b3f41bae0f3ed0b5bfc3ab1
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-10-02 12:21:29 +0500

    Импортировал файлы

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,21 @@
 import "./styles/_reboot.scss";
 import "./styles/_container.scss";
 import "./styles/_variables.scss";
+import "./styles/_visually-hidden.scss";
 import "./styles/_fonts.scss";
 import "./styles/_btn.scss";
 import "./Components/Antd/Antd.scss";
 
 import { Header } from "./Components/Header/Header";
 import { Breadcrumbs } from "./Components/Breadcrumbs/Breadcrumbs";
+import { Catalog } from "./Components/Catalog/Catalog";
 
 function App() {
   return (
     <>
       <Header />
       <Breadcrumbs />
+      <Catalog />
     </>
   );
 }
