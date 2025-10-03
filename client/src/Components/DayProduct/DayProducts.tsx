e5d5129ef9056e848dab6cda1b9f019fc4835592
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-10-03 12:17:49 +0500

    Вывел товары дня

--- client/src/Components/DayProduct/DayProducts.tsx
+++ client/src/Components/DayProduct/DayProducts.tsx
@@ -1,35 +1,58 @@
-import "./DayPproducts.scss";
+import { useEffect, useState } from "react";
+import "./DayProducts.scss";
+import type { ProductCard } from "../Types/Types";
+import { Card } from "../Card/Card";
 
 export const DayProduct = () => {
+  const [products, setProducts] = useState<ProductCard[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/lamps")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.log("Ошибка:", err));
+  }, []);
+
   return (
     <section className="day-products">
-    <div className="container">
-      <div className="day-products__top">
-        <h2 className="day-products__title">Товары дня</h2>
+      <div className="container">
+        <div className="day-products__top">
+          <h2 className="day-products__title">Товары дня</h2>
 
           {/* Навигация для слайдера */}
-        <div className="day-products__navigation">
-          <button className="day-products__navigation-btn day-products__navigation-btn--prev swiper-button-next" disabled type="button">
-            <svg width="41" height="9" aria-hidden="true">
-              <use href="images/sprite.svg#icon-arrow-prev"></use>
-            </svg>
-          </button>
-          <button className="day-products__navigation-btn day-products__navigation-btn--next swiper-button-prev" type="button">
-            <svg width="41" height="9" aria-hidden="true">
-              <use href="images/sprite.svg#icon-arrow-next"></use>
-            </svg>
-          </button>
+          <div className="day-products__navigation">
+            <button
+              className="day-products__navigation-btn day-products__navigation-btn--prev swiper-button-next"
+              disabled
+              type="button"
+            >
+              <svg width="41" height="9" aria-hidden="true">
+                <use href="images/sprite.svg#icon-arrow-prev"></use>
+              </svg>
+            </button>
+            <button
+              className="day-products__navigation-btn day-products__navigation-btn--next swiper-button-prev"
+              type="button"
+            >
+              <svg width="41" height="9" aria-hidden="true">
+                <use href="images/sprite.svg#icon-arrow-next"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        {/* <!-- Slider main container --> */}
+        <div className="swiper">
+          {/* <!-- Additional required wrapper --> */}
+          <div className="swiper-wrapper">
+            {products
+              .filter((item) => item.goodsOfDay)
+              .map((item) => (
+                <Card key={item.id} {...item} />
+              ))}
+          </div>
         </div>
       </div>
-      
-      {/* <!-- Slider main container --> */}
-      <div className="swiper">
-        {/* <!-- Additional required wrapper --> */}
-        <div className="swiper-wrapper">
-          {/* <!-- Slides --> */}
-        </div>
-      </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
